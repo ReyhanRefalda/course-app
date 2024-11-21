33c52d350414c06a5782626b950ecb362e566083
ReyhanRefalda <repaldaganss@gmail.com>
2024-11-21 15:10:25 +0700

    menyesuaikan edit artikel

--- routes/web.php
+++ routes/web.php
@@ -24,38 +24,42 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::prefix('admin')->name('admin.')->group(function(){
+    Route::prefix('admin')->name('admin.')->group(function () {
 
-        Route::middleware('can:manage kursus')->group(function(){
+        Route::middleware('can:manage kursus')->group(function () {
             Route::resource('kursus', KursusController::class);
         });
 
-        Route::middleware('can:manage modul')->group(function(){
+        Route::middleware('can:manage modul')->group(function () {
             Route::resource('modul', AdminModulController::class);
         });
 
-        Route::middleware('can:manage pelajaran')->group(function(){
+        Route::middleware('can:manage pelajaran')->group(function () {
             Route::resource('pelajaran', AdminPelajaranController::class);
         });
 
-        Route::middleware('can:manage artikel')->group(function(){
+        Route::middleware('can:manage artikel')->group(function () {
             Route::resource('artikel', AdminArtikelController::class);
         });
 
-        Route::middleware('can:manage user')->group(function(){
+        Route::middleware('can:manage user')->group(function () {
             Route::get('user', [AdminController::class, 'listUsers'])->name('user.index');
             Route::get('/users/{id}/edit', [AdminController::class, 'editUser'])->name('users.edit');
             Route::patch('/users/{id}/updateUser', [AdminController::class, 'updateUser'])->name('users.updateUser');
             Route::delete('/users/{id}/destroyUser', [AdminController::class, 'destroyUser'])->name('users.destroyUser');
         });
     });
-
-
 });
 
+require __DIR__ . '/auth.php';
